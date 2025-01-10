388cf710033e0a471396dcd810bee1afb40094a5
sdoering01 <57443123+sdoering01@users.noreply.github.com>
2025-01-10 00:21:30 +0000

    add scroll restoration on post-redirect-get

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,6 +22,10 @@ import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
+import {initScrollRestoration} from "./restore-scroll"
+
+initScrollRestoration();
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {
   longPollFallbackMs: 2500,
@@ -41,4 +45,3 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
-
